d7e51f2f86b4dce4419b07c745330559c4203f4f
a00248562 <a00248562@Z205-01.studentnet.ait.ie>
2018-10-17 15:50:57 +0100

    test3

--- ClickNGo/source/Controller/HolidayServlet.java
+++ ClickNGo/source/Controller/HolidayServlet.java
@@ -29,21 +29,21 @@ public class HolidayServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-		System.out.println("checkin for ID in db");
+		//System.out.println("checkin for ID in db");
 		
-		int ID = request.getContentLength();
-		String Destination=request.getParameter("Destination");
-		String StartData=request.getParameter("StartData");
-		String EndData=request.getParameter("EndData");
-		int Cost=request.getContentLength();
-		int Quantity=request.getContentLength();
+		//int ID = request.getContentLength();
+		//String Destination=request.getParameter("Destination");
+		//String StartData=request.getParameter("StartData");
+		//String EndData=request.getParameter("EndData");
+		//int Cost=request.getContentLength();
+		//int Quantity=request.getContentLength();
 	//	HolidayDAO.instance
-		request.getRequestDispatcher("addHoliday.jsp").forward(request, response);
-	}
+		//request.getRequestDispatcher("addHoliday.jsp").forward(request, response);
+	//}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
@@ -56,10 +56,15 @@ public class HolidayServlet extends HttpServlet {
 //		String name = request.getParameter("name");
 //		p.println("<p>hello"+name+"</p>");
 //		p.close();
-		//String name = request.getParameter("name");
-		//Name A_Name= new Name(name);
-		//HolidayDAO.instance.saveFirstName(A_Name);
-		//request.getRequestDispatcher("addHoliday.jsp").forward(request, response);
+		int ID = Integer.parseInt(request.getParameter("ID"));
+		String Destination =request.getParameter("Destination");
+		String StartData =request.getParameter("StartData");
+		String EndData =request.getParameter("EndData");
+		int Cost = Integer.parseInt(request.getParameter("Cost"));
+		int Quantity = Integer.parseInt(request.getParameter("Quantity"));
+		holiday newholiday=new holiday(ID,Destination,StartData,EndData,Cost,Quantity);
+		HolidayDAO.instance.addHoliday(newholiday);
+		request.getRequestDispatcher("addHoliday.jsp").forward(request, response);
 		
 	}
 
--- ClickNGo/source/DAO/HolidayDAO.java
+++ ClickNGo/source/DAO/HolidayDAO.java
@@ -27,78 +27,24 @@ public enum HolidayDAO {
 		return connection;
 	}
 	
-	public void saveID(holiday id) {
+	public void addHoliday(holiday newholiday) {
 		Connection connection = getConnection();
 		try {
-			PreparedStatement psmt = connection.prepareStatement("INSERT INTO holidayAvailable (ID) VALUES(?)");
-			psmt.setInt(1,id.getID());
+			PreparedStatement psmt = connection.prepareStatement("INSERT INTO holiday (ID, Destination, StartData, EndData, Cost, Quantity) VALUES(?,?,?,?,?,?)");
+			psmt.setInt(1,newholiday.getID());
+			psmt.setString(2,newholiday.getDestination());
+			psmt.setString(3,newholiday.getStartData());
+			psmt.setString(4,newholiday.getEndData());
+			psmt.setInt(5,newholiday.getCost());
+			psmt.setInt(6,newholiday.getQuantity());
 			psmt.executeUpdate();
-			System.out.println("Added" + id.getID()+"to the database.");
+			System.out.println("Added " + newholiday.getID()+" to the database.");
 		}catch(SQLException e) {
 			e.printStackTrace();
-		}
-		
+		}	
 	}
-	public void saveDestination(holiday d) {
-		Connection connection = getConnection();
-		try {
-			PreparedStatement psmt = connection.prepareStatement("INSERT INTO holiday Available (Destination) VALUES(?)");
-			psmt.setString(1,d.getDestination());
-			psmt.executeUpdate();
-			System.out.println("Added" + d.getDestination()+"to the database.");
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	public void saveStartData(holiday sd) {
-		Connection connection = getConnection();
-		try {
-			PreparedStatement psmt = connection.prepareStatement("INSERT INTO holiday Available (StartData) VALUES(?)");
-			psmt.setString(1,sd.getStartData());
-			psmt.executeUpdate();
-			System.out.println("Added" + sd.getStartData()+"to the database.");
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	public void saveEndData(holiday ed) {
-		Connection connection = getConnection();
-		try {
-			PreparedStatement psmt = connection.prepareStatement("INSERT INTO holiday Available (EndData) VALUES(?)");
-			psmt.setString(1,ed.getEndData());
-			psmt.executeUpdate();
-			System.out.println("Added" + ed.getEndData()+"to the database.");
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	public void saveCost(holiday c) {
-		Connection connection = getConnection();
-		try {
-			PreparedStatement psmt = connection.prepareStatement("INSERT INTO holiday Available (Cost) VALUES(?)");
-			psmt.setInt(1,c.getCost());
-			psmt.executeUpdate();
-			System.out.println("Added" + c.getCost()+"to the database.");
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	public void saveQuantity(holiday q) {
-		Connection connection = getConnection();
-		try {
-			PreparedStatement psmt = connection.prepareStatement("INSERT INTO holiday Available (Quantity) VALUES(?)");
-			psmt.setInt(1,q.getQuantity());
-			psmt.executeUpdate();
-			System.out.println("Added" + q.getQuantity()+"to the database.");
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
+	
+	
 	
 	/*public Name checkFirstName(String name) {
 		Connection connection = getConnection();
