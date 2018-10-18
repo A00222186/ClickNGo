4f37dfbb4b92fad4279c226348ce5bc7543b358d
Balji <Balji@172.16.21.165>
2018-10-18 14:02:47 +0100

    Remove Holiday story complete.

---
+++ ClickNGo/src/dao/removeHolDAO.java
@@ -0,0 +1,39 @@
+package dao;
+
+import model.*;
+import java.sql.*;
+
+public enum removeHolDAO {
+	
+	instance;
+	public Connection getConnection(){
+	Connection connection = null;
+	try {
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		connection = DriverManager.getConnection("jdbc:mysql://Localhost:3306/clickngodb", "root", "admin");
+				if(connection !=null) {
+				System.out.println("Connected to ClickNGoDB OK!");	
+				}
+	}catch(Exception e) {
+			System.out.println("Couldnt connect");
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
+	public void remHoliday(int IDtoRemove)
+	{
+		Connection connection = getConnection();
+		try {
+			PreparedStatement psmt = connection.prepareStatement("Delete from holidaytable where ID = ?");
+			psmt.setInt(1, IDtoRemove);
+			psmt.executeUpdate();
+			System.out.println(IDtoRemove + " Holiday removed from DB");
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Could not remove ID from DB");
+		}
+	}
+
+}
